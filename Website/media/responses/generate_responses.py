c0cfe8c6014d7ceca94d13e3957e59628576b3b6
CreeptonThingz <creepton@outlook.com>
2021-11-08 21:57:37 -0700

    Move generate_responses.py

--- Website/media/responses/generate_responses.py
+++
@@ -1,9 +0,0 @@
-import torch
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='/media/yolov5_weights.pt')
-
-#Function takes in an image and returns a json file containing prediction
-def generate_response(img_path): 
-  results = model(img_path)
-  df = results.pandas().xyxy[0]
-  json_file = df.to_json()
-  return json_file
