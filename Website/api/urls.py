2e3d6fc24819b20e46dc99a5e2207f3b8c7f586c
CreeptonThingz <creepton@outlook.com>
2021-11-08 22:21:25 -0700

    Change view

--- Website/api/urls.py
+++ Website/api/urls.py
@@ -4,5 +4,5 @@ from .views import TextView, ImageUploadView, SendMessageView
 urlpatterns = [
     path('', TextView.as_view()),
     path('text/', SendMessageView.as_view()),
-    path('post/', ImageUploadView.as_view(), name="image_posts")
+    path('post/', ImageUploadView.as_view())
 ]
