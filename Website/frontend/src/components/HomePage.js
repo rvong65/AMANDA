d9991a176dbd4b550309c65cb25f29c42e16106d
CreeptonThingz <creepton@outlook.com>
2021-11-08 22:18:30 -0700

    Small cleanup

--- Website/frontend/src/components/HomePage.js
+++ Website/frontend/src/components/HomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import AboutUs from "./AboutUs";
 import Message from "./Message";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
-import { Input, Button, Grid, Typography, TextField, FormHelperText, FormControl } from "@material-ui/core";
 import axios from "axios";
 
 export default class HomePage extends Component {
@@ -13,15 +12,35 @@ export default class HomePage extends Component {
                 username: "Medical Assistant",
                 content: <p>Hello</p>
             }],
-            image: null
+            image: null,
         };
 
-        this.submitMessage = this.submitMessage.bind(this);
-        this.imageChange = this.imageChange.bind(this);
+        this.handleSendMessagePressed = this.handleSendMessagePressed.bind(this);
         this.printState = this.printState.bind(this);
-        // this.handleTextInput = this.handleTextInput.bind(this);
-        // this.handleSendMessagePressed = this.handleSendMessagePressed.bind(this);
         this.handleImageUpload = this.handleImageUpload.bind(this);
+    }
+
+    componentDidMount() {
+        this.scrollToBot();
+    }
+
+    componentDidUpdate() {
+        this.scrollToBot();
+    }
+
+    // Automatic scroll to bottom of chat
+    scrollToBot() {
+        document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
+    }
+
+    printState() {
+        console.log(this.state);
+    }
+
+    getMessageData() {
+        fetch("/api/post/") // need to change to text model
+            .then((response) => response.json())
+            .then((data) => console.log(data)); 
     }
 
     submitMessage(e) {
@@ -39,48 +58,31 @@ export default class HomePage extends Component {
         }
     }
 
-    componentDidMount() {
-        this.scrollToBot();
+    handleSendMessagePressed(e) {
+        e.preventDefault();
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                user_msg: this.state.user_msg,
+            })
+        };
+
+        fetch('/api/text/', requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
     }
-
-    componentDidUpdate() {
-        this.scrollToBot();
-    }
-
-    scrollToBot() {
-        document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
-    }
-
-    imageChange(e) {
-    }
-
-    printState() {
-        console.log(this.state);
-    }
-
-    // handleSendMessagePressed(e) {
-    //     const requestOptions = {
-    //         method: 'POST',
-    //         headers: {'Content-Type': 'application/json'},
-    //         body: JSON.stringify({
-    //             user_msg: this.state.user_msg,
-    //         })
-    //     };
-
-    //     fetch('/api/text', requestOptions)
-    //         .then((response) => response.json())
-    //         .then((data) => console.log(data));
-    // }
 
     handleImageUpload = async (e) => {
         e.preventDefault();
         await this.setState({ image: e.target.files[0] });
 
-        console.log(this.state);
-        let form_data = new FormData();
-        form_data.append('image', this.state.image, this.state.image.name);
-        let url = '/api/post/';
-        axios.post(url, form_data, {
+        let formData = new FormData();
+        formData.append('image', this.state.image);
+        formData.append('title', this.state.image.name);
+
+        await axios.post("/api/post/", formData, {
           headers: {
             'content-type': 'multipart/form-data'
           }
@@ -88,7 +90,7 @@ export default class HomePage extends Component {
             .then(res => {
               console.log(res.data);
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
       };
 
     render() {
@@ -104,14 +106,17 @@ export default class HomePage extends Component {
                             <ul className="messages" id="messages">
                                 {messages.map((message) => <Message message={message} user={username} />)}
                             </ul>
-                            <form className="input" onSubmit={this.submitMessage}>
+                            <form onSubmit={this.handleSendMessagePressed}>
                                 <input id="user-input" type="text" ref="msg" />
                                 <button type="submit">Send</button>
+                            </form>
+                            <div className="input">
                                 <input type="file" accept="image/png, image/jpeg" hidden id="image-upload" onChange={this.handleImageUpload} />
                                 <button onClick={() => document.getElementById("image-upload").click()} id="image-upload-button">Upload</button>
-                            </form>
+                            </div>
                         </div>
                         <button onClick={this.printState}>State</button>
+                        <button onClick={this.getMessageData}>API</button>
                     </Route>
                     <Route path='/about' component={AboutUs} />
                 </Switch>
