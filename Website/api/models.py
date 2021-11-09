8cb31c7fd608c829d9382a57b8d794a2890b6d51
CreeptonThingz <creepton@outlook.com>
2021-11-08 22:20:46 -0700

    Update variable names

--- Website/api/models.py
+++ Website/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class TextArea(models.Model):
-    user_msg = models.CharField(max_length=500, default="")
-    # upload_img = models.FileField(upload_to='uploads/% Y/% m/% d/', default="")
+    message = models.CharField(max_length=500, default="")
 
 class ImageUpload(models.Model):
+    title = models.CharField(max_length=500, default="")
     image = models.ImageField(upload_to='post_images')
