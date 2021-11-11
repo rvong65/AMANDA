fe7befd36c012babec26ed9711e57b6be90502fb
CreeptonThingz <creepton@outlook.com>
2021-11-11 07:52:26 -0700

    Update submitReport function

--- Website/frontend/src/components/HomePage.js
+++ Website/frontend/src/components/HomePage.js
@@ -31,34 +31,17 @@ export default class HomePage extends Component {
         document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
     }
 
+    // Get Response from API and call submitReport()
     useMessageData() {
-        fetch("/api/") // need to change to text model
+        fetch("/api/")
             .then((response) => response.json())
-            .then((data) => this.submitReport(data)); 
+            .then((data) => this.submitReport(data.name, data.confidence));  
     }
-
-//     submitReport(messageContent) {
-//         this.setState({
-//             messages: this.state.messages.concat([{
-//                 username: "Medical Assistant",
-//                 content: <p>{messageContent}</p>,
-//             }])
-//         });
-//     }
     
     submitReport(name, confidence) {
-        var response = "I did not detect any infections or burns.";
-        var percent = confidence * 100 // I'm assuming that confidence is a value between 0-1, so this just converts to percentage.
-        
-//         if(name.toLowerCase() == "cellulitis") {
-//             response = "I\'ve detected a cellulitis infection with " + percent + "% accuracy. Here are some treatment plans:\n\n(1) Cellulitis treatment usually includes a prescription oral antibiotic. Within 3 days of starting an antibiotic, let your doctor know whether the infection is responding to treatment. You’ll need to take the antibiotic for as long as your doctor directs (usually 5 to 10 days).\n\n(2) In the meantime, try these steps to ease any pain and swelling:\n- Place a cool, damp cloth on the affected area as often as needed for your comfort.\n- Ask your doctor to suggest an over-the-counter pain medication to treat pain.\n- Elevate the affected part of your body.\n- Ask your doctor whether it might help to wear compression wraps or stockings.";
-//         } else if(name.toLowerCase() == "first degree burn") {
-//             response = "I\'ve detected a first-degree burn with " + percent + "% accuracy. Here are some treatment plans:\n\n(1) Cool the burn. Immediately immerse the burn in cool tap water or apply cold, wet compresses.\n(2) Apply petroleum jelly two to three times daily.\n(3) Cover the burn with a nonstick, sterile bandage.\n(4) Consider taking over-the-counter pain medication.\n(5) Protect the area from the sun.";
-//         } else if(name.toLowerCase() == "second degree burn") {
-//             response = "I\'ve detected a second-degree burn with " + percent + "% accuracy. Here are some treatment plans:\n\n(1) Rinse the burn with cool water until the pain stops (15-30 minutes). Do not use ice or ice water, which can cause tissue damage.\n(2) Clean the burn. Do not touch the burn with your hands or anything dirty, because open blisters can easily be infected.\n(3) Bandaging the burn. No need to do this if the burned skin or blister is not broken open.\n(4) If the burn is on a leg or an arm, keep the limb raised as much as possible for the first 24 to 48 hours to decrease swelling.";
-//         } else if(name.toLowerCase() == "third degree burn") {
-//             response = "I\'ve detected a third-degree burn with " percent + "% accuracy. Here are some treatment plans:\n\n(1) Surgery: Third degree burns typically require multiple surgeries to remove burned tissue from the burn site.\n(2) Skin graft: As third degree burns do not heal by themselves, a skin graft is often necessary. A doctor may use a combination of natural skin grafts, artificial skin products, or laboratory-grown skin.\n(3) Intravenous fluids: Some people may receive extra fluids to maintain their blood pressure and prevent shock.\n(4) Medication: A person will likely receive several different medications, such as antibiotics and pain medication, to prevent infection and ease pain.\n(5) Tetanus shot: As tetanus bacteria are more likely to trigger infections through burn wounds, a person may receive a tetanus shot to prevent this.";
-//         }
+        var response;
+        var percent = parseInt(confidence * 100) // I'm assuming that confidence is a value between 0-1, so this just converts to percentage as an integer.
+
         switch(name.toLowerCase()) {
             case "cellulitis":
                 response = "I\'ve detected a cellulitis infection with " + percent + "% accuracy. Here are some treatment plans:\n\n(1) Cellulitis treatment usually includes a prescription oral antibiotic. Within 3 days of starting an antibiotic, let your doctor know whether the infection is responding to treatment. You’ll need to take the antibiotic for as long as your doctor directs (usually 5 to 10 days).\n\n(2) In the meantime, try these steps to ease any pain and swelling:\n- Place a cool, damp cloth on the affected area as often as needed for your comfort.\n- Ask your doctor to suggest an over-the-counter pain medication to treat pain.\n- Elevate the affected part of your body.\n- Ask your doctor whether it might help to wear compression wraps or stockings.";
@@ -73,7 +56,7 @@ export default class HomePage extends Component {
                 response = "I\'ve detected a third-degree burn with " + percent + "% accuracy. Here are some treatment plans:\n\n(1) Surgery: Third degree burns typically require multiple surgeries to remove burned tissue from the burn site.\n(2) Skin graft: As third degree burns do not heal by themselves, a skin graft is often necessary. A doctor may use a combination of natural skin grafts, artificial skin products, or laboratory-grown skin.\n(3) Intravenous fluids: Some people may receive extra fluids to maintain their blood pressure and prevent shock.\n(4) Medication: A person will likely receive several different medications, such as antibiotics and pain medication, to prevent infection and ease pain.\n(5) Tetanus shot: As tetanus bacteria are more likely to trigger infections through burn wounds, a person may receive a tetanus shot to prevent this.";
                 break;
             default:
-                "I did not detect any infections or burns.";
+                response = "I did not detect any infections or burns.";
         }
         
         // this part is just modeled after the original submitReport function (commented above)
@@ -105,7 +88,7 @@ export default class HomePage extends Component {
             })
             .catch(err => console.log(err));
 
-        // Send "Uploaded (image name) message"
+        // Send "Uploaded (image name)" message
         this.setState({
             messages: this.state.messages.concat([{
                 username: "User",
@@ -114,8 +97,11 @@ export default class HomePage extends Component {
         });
 
         // Send message on message area
-        this.useMessageData();
-      };
+        await this.useMessageData();
+
+        // Clear image state
+        this.setState({ image: null });
+    }
 
     render() {
         const username = "User";
