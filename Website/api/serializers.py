bdead8a7bc860aafc2694cfa4edfaf47a1d97711
rvong65 <87782709+rvong65@users.noreply.github.com>
2021-11-12 15:06:46 -0800

    Update serializers.py

--- Website/api/serializers.py
+++ Website/api/serializers.py
@@ -2,6 +2,16 @@ from typing import Text
 from rest_framework import serializers
 from .models import ImageUpload
 
+class TextSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TextArea
+        fields = ('id', 'message')
+
+class SendMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TextArea
+        fields = '__all__'
+        
 class ImageUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImageUpload
