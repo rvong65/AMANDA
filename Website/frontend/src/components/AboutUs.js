3593774b95e651db07e13138974027b50aa9136b
CreeptonThingz <creepton@outlook.com>
2021-11-15 15:39:13 -0700

    Update AboutUs.js

--- Website/frontend/src/components/AboutUs.js
+++ Website/frontend/src/components/AboutUs.js
@@ -12,31 +12,29 @@ export default class AboutUs extends Component {
                 <Navbar />
                 <div class="about-section">
                     <h1>About Us Page</h1>
-                    <p>INTRODUCING TEAM X-MEN</p>
+                    <p>INTRODUCING TEAM XMEN</p>
                 </div>
-                {/* 
 
-                <! -- Farzaan Wadiwalla -->
-                <h2 style="text-align:center">Our Team</h2>
+                <h2 class="our-team">Our Team</h2>
                 <div class="row">
+                    {/* <! -- Farzaan Wadiwalla --> */}
                     <div class="column">
                         <div class="card">
-                        <img src="https://media.discordapp.net/attachments/893914165884882994/909167195249057873/IMG_9805.PNG?width=334&height=593" width = "190" height = "300" alt="Farzaan" />   <! -- after alt: style="width:100%" -->
-                        <div class="container">
-                            <h2>Farzaan Wadiwalla</h2>
-                            <p class="title">Data Analyst</p>
-                            <br />Farzaan is a junior at Lincoln Highschool in Portland, Oregon. He is fascinated by the world of artificial intelligence and hopes to further his knowledge in AI-technology to solve real-world problems.
-                            <br /><br />farzaanw@gmail.com
-                            <br /><br /><button class="button">Contact</button>
-                        </div> 
+                            <img class="profile-picture" src="https://media.discordapp.net/attachments/893914165884882994/909167195249057873/IMG_9805.PNG?width=334&height=593" alt="Farzaan" />
+                            <div class="container">
+                                <h2>Farzaan Wadiwalla</h2>
+                                <p class="title">Data Analyst</p>
+                                <br />Farzaan is a junior at Lincoln Highschool in Portland, Oregon. He is fascinated by the world of artificial intelligence and hopes to further his knowledge in AI-technology to solve real-world problems.
+                                <br /><br />farzaanw@gmail.com
+                                <br /><br /><button class="button">Contact</button>
+                            </div> 
+                        </div>
                     </div>
-                </div>
-                {/* 
 
-                <! -- Daniel Gao -->
+                    {/* <! -- Daniel Gao --> */}
                     <div class="column">
                         <div class="card">
-                            <img src ="https://cdn.discordapp.com/attachments/885725394110644224/909133221889982524/profile_photo.png" width = "240" height = "300" alt="Daniel" />   <! -- after alt: style="width:100%" -->
+                            <img class="profile-picture" src="https://cdn.discordapp.com/attachments/885725394110644224/909133221889982524/profile_photo.png" alt="Daniel" />
                             <div class="container">
                                 <h2>Daniel Gao</h2>
                                 <p class="title">Product Manager</p>
@@ -46,27 +44,11 @@ export default class AboutUs extends Component {
                             </div>
                         </div>
                     </div>
-                    {/* 
-                    
-                <! -- Ryan Vong -->
-                    <div class="column">
-                        <div class="card">
-                            <img src="https://cdn.discordapp.com/attachments/886044529881403462/909634364856479815/IMG-d04dfff8de2d36484fccd20c50228e63-V.jpg" alt="Ryan" width = "190" height = "300" />
-                            <div class="container">
-                                <h2>Ryan Vong</h2>
-                                <p class="title">Software Engineer</p>
-                                <br />Ryan is a high school senior who resides in Los Angeles, California. In his free time, he usually reads sci-fi books and overall he has a spontaneous personality who likes trying new things.
-                                <br /><br />ryanvong8@gmail.com
-                                <br /><br /><button class="button">Contact</button>
-                            </div>
-                        </div>
-                    </div>
-                    {/* 
 
-                <! -- Jasper Shen -->
+                    {/* <! -- Jasper Shen --> */}
                     <div class="column">
                         <div class="card">
-                            <img src="https://media.discordapp.net/attachments/715017264361111563/867076500368785478/unknown.png?width=728&height=713" alt="Jasper" width = "260" height = "300" />
+                            <img class="profile-picture" src="https://media.discordapp.net/attachments/715017264361111563/867076500368785478/unknown.png?width=728&height=713" alt="Jasper" />
                             <div class="container">
                                 <h2>Jasper Shen</h2>
                                 <p class="title">Product Designer</p>
@@ -78,25 +60,38 @@ export default class AboutUs extends Component {
                     </div>
                     {/* 
 
-                <! -- Nick Goodchild -->
+                    {/* <! -- Ryan Vong --> */}
                     <div class="column">
                         <div class="card">
-                            <img src="https://cdn.discordapp.com/attachments/886044587712450630/909635675853324288/GoodchildNicholasSenior.jpg" alt="Nick" width = "250" height = "300" />
+                            <img class="profile-picture" src="/w3images/team3.jpg" alt="Ryan" />
+                            <div class="container">
+                                <h2>Ryan Vong</h2>
+                                <p class="title">Software Engineer</p>
+                                <br />Ryan is a high school senior who resides in Los Angeles, California. In his free time, he usually reads sci-fi books and overall he has a spontaneous personality who likes trying new things.
+                                <br /><br />ryanvong8@gmail.com
+                                <br /><br /><button class="button">Contact</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <! -- Nick Goodchild --> */}
+                    <div class="column">
+                        <div class="card">
+                            <img class="profile-picture" src="https://cdn.discordapp.com/attachments/886044587712450630/909174573071761438/default-profile-icon-24.png" alt="Nick" />
                             <div class="container">
                                 <h2>Nick Goodchild</h2>
                                 <p class="title">Software Engineer</p>
-                                <br />Nick is a high school senior from a town in the middle of nowhere; Ashburnham, Massachusetts. He loves all things STEM, and is planning on pursuing a degree in Robotics Engineering and/or Math Education.
+                                <br />Add some text description
                                 <br /><br />nickgoodchild654@gmail.com
                                 <br /><br /><button class="button">Contact</button>
                             </div>
                         </div>
                     </div>
-                    {/* 
 
-                <! -- Cameron Jackson -->
+                    {/* <! -- Cameron Jackson --> */}
                     <div class="column">
                         <div class="card">
-                            <img src="https://media.discordapp.net/attachments/885725394110644224/909154845120946186/Screen_Shot_2021-11-13_at_10.56.41_AM.png" alt="Cameron" width = "260" height = "300" />
+                            <img class="profile-picture" src="https://media.discordapp.net/attachments/885725394110644224/909154845120946186/Screen_Shot_2021-11-13_at_10.56.41_AM.png" alt="Cameron" />
                             <div class="container">
                                 <h2>Cameron Jackson</h2>
                                 <p class="title">Our Mentor</p>
@@ -106,6 +101,7 @@ export default class AboutUs extends Component {
                         </div>
                     </div>
                 </div>
-                </>
-                )};
-                }
+            </>
+        );
+    }
+}
