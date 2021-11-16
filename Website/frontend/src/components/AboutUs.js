6eec9c31ea545e35a2ef349ba1497ea54183f5b8
Daniel Gao <61636390+danielgao20@users.noreply.github.com>
2021-11-15 20:28:40 -0800

    Update AboutUs.js

--- Website/frontend/src/components/AboutUs.js
+++ Website/frontend/src/components/AboutUs.js
@@ -23,7 +23,7 @@ export default class AboutUs extends Component {
                             <img class="profile-picture" src="https://media.discordapp.net/attachments/893914165884882994/909167195249057873/IMG_9805.PNG?width=334&height=593" alt="Farzaan" />
                             <div class="container">
                                 <h2>Farzaan Wadiwalla</h2>
-                                <p class="title">Data Analyst</p>
+                                <p class="title">Product Analyst</p>
                                 <br />Farzaan is a junior at Lincoln Highschool in Portland, Oregon. He is fascinated by the world of artificial intelligence and hopes to further his knowledge in AI-technology to solve real-world problems.
                                 <br /><br />farzaanw@gmail.com
                                 <br /><br /><button class="button">Contact</button>
@@ -37,7 +37,7 @@ export default class AboutUs extends Component {
                             <img class="profile-picture" src="https://cdn.discordapp.com/attachments/885725394110644224/909133221889982524/profile_photo.png" alt="Daniel" />
                             <div class="container">
                                 <h2>Daniel Gao</h2>
-                                <p class="title">Product Manager</p>
+                                <p class="title">Product Lead</p>
                                 <br />Daniel is a high school student from San Francisco who is interested in CS and Math. He hopes to explore the applications of AI and use them to solve problems in the world.
                                 <br /><br />dgao22@siprep.org
                                 <br /><br /><button class="button">Contact</button>
@@ -66,7 +66,7 @@ export default class AboutUs extends Component {
                             <img class="profile-picture" src="https://cdn.discordapp.com/attachments/886044529881403462/910008541169995796/Screenshot_2021-11-15_192817.png" alt="Ryan" />
                             <div class="container">
                                 <h2>Ryan Vong</h2>
-                                <p class="title">Software Engineer</p>
+                                <p class="title">Product Engineer</p>
                                 <br />Ryan is a high school senior who resides in Los Angeles, California. In his free time, he usually reads sci-fi books and overall he has a spontaneous personality who likes trying new things.
                                 <br /><br />ryanvong8@gmail.com
                                 <br /><br /><button class="button">Contact</button>
@@ -80,7 +80,7 @@ export default class AboutUs extends Component {
                             <img class="profile-picture" src="https://cdn.discordapp.com/attachments/886044587712450630/909174573071761438/default-profile-icon-24.png" alt="Nick" />
                             <div class="container">
                                 <h2>Nick Goodchild</h2>
-                                <p class="title">Software Engineer</p>
+                                <p class="title">Product Engineer</p>
                                 <br />Nick is a high school senior from a town in the middle of nowhere; Ashburnham, Massachusetts. He loves all things STEM, and is planning on pursuing a degree in Robotics Engineering and/or Math Education.
                                 <br /><br />nickgoodchild654@gmail.com
                                 <br /><br /><button class="button">Contact</button>
@@ -94,7 +94,7 @@ export default class AboutUs extends Component {
                             <img class="profile-picture" src="https://media.discordapp.net/attachments/885725394110644224/909154845120946186/Screen_Shot_2021-11-13_at_10.56.41_AM.png" alt="Cameron" />
                             <div class="container">
                                 <h2>Cameron Jackson</h2>
-                                <p class="title">Our Mentor</p>
+                                <p class="title">Mentor</p>
                                 <br />Cameron Jackson attends the University of Texas at Dallas as a Software Engineering major. Cameron enjoys knitting, playing chess and learning about cutting edge AI in his free time. He is deeply interested in way too many different fields of AI and sharing this passion with everyone within earshot.
                                 <br /><br /><button class="button">Contact</button>
                             </div>
