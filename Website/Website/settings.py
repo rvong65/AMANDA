d2d0c2d4a223620130ad07d45cd807b2edfde1b6
Ryan Vong <87782709+rvong65@users.noreply.github.com>
2021-11-22 18:42:58 -0800

    Update settings.py

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'api.apps.ApiConfig',
     'rest_framework',
     'corsheaders',
@@ -60,7 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+
 
 ROOT_URLCONF = 'Website.urls'
 
